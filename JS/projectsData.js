e282ab1bb5499f351c91e573fed6fab3fcd0b3cf
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2019-04-29 09:22:10 -0500

    working on the images.

--- JS/projectsData.js
+++ JS/projectsData.js
@@ -1,102 +1,101 @@
 const PROJECTS = {
-    projects: [
-        {
-            title: "Buy-a-Ride",
-            projectLink: "https://github.com/Dannyp123/Django-Car-Store",
-            langs: "HTML/CSS, Python, Django, SQite, Bootstrap, Materilized CSS",
-            description: `
+  projects: [
+    {
+      title: "Buy-a-Ride",
+      projectLink: "https://github.com/Dannyp123/Django-Car-Store",
+      langs: "HTML/CSS, Python, Django, SQite, Bootstrap, Materilized CSS",
+      description: `
             This independent application is a Django, full stack application. It shows off cars for sale and the details for each. The goal of this application was to get more independent work under my belt, and I want to dedicate more time to development outside of school. 
             `,
-            img: "/images/Screenshot_2019-04-12 Buy-A-Ride.png"
-        },
-        {
-            title: "BCCA Job Posting",
-            projectLink: "https://github.com/Dannyp123/Base-Camp-Job-Search",
-            langs: "HTML/CSS, JavaScript, Python, Django, SQlite, Bootstrap",
-            description: `
+      img: "/images/Screenshot_2019-04-12 Buy-A-Ride.png"
+    },
+    {
+      title: "BCCA Job Posting",
+      projectLink: "https://github.com/Dannyp123/Base-Camp-Job-Search",
+      langs: "HTML/CSS, JavaScript, Python, Django, SQlite, Bootstrap",
+      description: `
             This is a application that is the second to last project in Base Camp. It is done in Django. It allows a user to read job postings, read comments for each, and the details for it. An admin can make jobs, comment on jobs, and delete jobs.
 
             `,
-            img: "/images/Screenshot from 2019-04-01 13-36-08.png"
-        },
+      img: "/images/Screenshot from 2019-04-01 13-36-08.png"
+    },
 
-        {
-            title: "BCCA Student Applying Application",
-            projectLink: "https://github.com/Dannyp123/bcca-student-application-spring",
-            langs: "HTML, CSS, JavaScript, Java, Bootstrap, and Spring",
-            description: `
+    {
+      title: "BCCA Student Applying Application",
+      projectLink:
+        "https://github.com/Dannyp123/bcca-student-application-spring",
+      langs: "HTML, CSS, JavaScript, Java, Bootstrap, and Spring",
+      description: `
             This is a project where I was in a team of three people, and we where given a task of making a full stack application. Everything from a database to the templates to show the user.
             The app allows students to apply to Base Camp and then the administrator to see who has applied, whether they are eligable or not. We had 7 1/2 school days to complete and then we presented infront of the class.
             `,
-            img: "/images/bcca_homepage.png"
-        },
-        {
-            title: "ESPN Player Input Stats",
-            projectLink: "https://github.com/Dannyp123/ESPN-Statistic-Slid-Box",
-            langs: "HTML, CSS, JavaScript, and Bootstrap",
-            description: `
+      img: "/images/bcca_homepage.png"
+    },
+    {
+      title: "ESPN Player Input Stats",
+      projectLink: "https://github.com/Dannyp123/ESPN-Statistic-Slid-Box",
+      langs: "HTML, CSS, JavaScript, and Bootstrap",
+      description: `
                 In my coding class, we where allowed time to work on a project that we wanted to make, either using new technology, or using already know technology in a new way. My goal was to showcase my front-end passion with making a football stat input system that changes the DOM with the input values. 
             `,
-            img: "/images/Screenshot from 2019-02-21 11-23-22.png"
-        },
+      img: "/images/Screenshot from 2019-02-21 11-23-22.png",
+      pageLink: "https://dannyp123.github.io/ESPN-Statistic-Slid-Box/"
+    },
 
-
-
-        {
-            title: "Dunn's Country Store",
-            projectLink: "https://github.com/Dannyp123/Dunns-Country-Store",
-            langs: "HTML, CSS, Bootstrap",
-            description: `This is a webpage that I would say is my first real webpage. I decided to see if I could
+    {
+      title: "Dunn's Country Store",
+      projectLink: "https://github.com/Dannyp123/Dunns-Country-Store",
+      langs: "HTML, CSS, Bootstrap",
+      description: `This is a webpage that I would say is my first real webpage. I decided to see if I could
         make a webpage for my place of work. The reason why is because I needed the practice, there
         is not anything new online about the store I work at, and because I enjoy Front-end work.
         Hopefully, I can get it pushed on a real domain name.`,
-            img: "/images/Screenshot from 2019-02-21 11-26-46.png",
-            pageLink: "https://dannyp123.github.io/Dunns-Country-Store/"
-        },
-        {
-            title: "Blog Post",
-            projectLink: "https://github.com/Dannyp123/Blog-Post",
-            langs: "HTML, CSS, Django, Bootstrap",
-            description: `This is a project that me and a partner came together to make a blog post application. Using Django, we made the whole application. You can make a post and comment on that post or any other post.`,
-            postImg: "/images/Screenshot_2018-12-21 Django Blog.png",
-            postLink: "https://github.com/Dannyp123/Blog-Post"
-        },
+      img: "/images/Screenshot from 2019-02-21 11-26-46.png",
+      pageLink: "https://dannyp123.github.io/Dunns-Country-Store/"
+    },
+    {
+      title: "Blog Post",
+      projectLink: "https://github.com/Dannyp123/Blog-Post",
+      langs: "HTML, CSS, Django, Bootstrap",
+      description: `This is a project that me and a partner came together to make a blog post application. Using Django, we made the whole application. You can make a post and comment on that post or any other post.`,
+      postImg: "/images/Screenshot_2018-12-21 Django Blog.png",
+      postLink: "https://github.com/Dannyp123/Blog-Post"
+    },
 
-
-        {
-            title: "Tic-Tac-BOO",
-            projectLink: "https://github.com/Dannyp123/Tic-Tac-Toe",
-            langs: "HTML, CSS, JavaScript",
-            description: `This is a JavaScript program that I made that is supposed to be the game Tic-Tac-Toe. I made it a
+    {
+      title: "Tic-Tac-BOO",
+      projectLink: "https://github.com/Dannyp123/Tic-Tac-Toe",
+      langs: "HTML, CSS, JavaScript",
+      description: `This is a JavaScript program that I made that is supposed to be the game Tic-Tac-Toe. I made it a
         Halloween theme since when I made it, it was close to halloween. I used a spooky background image
         and unique game pieces. Using HTML
         and CSS to make it online and look good.`,
-            BooImg: "/images/Screenshot_2018-12-21 Tic Tac BOO.jpg",
-            BooLink: "https://dannyp123.github.io/Tic-Tac-BOO/"
-        },
-        {
-            title: "Temp-Converting",
-            projectLink: "https://dannyp123.github.io/Temp-Converting/",
-            langs: "HTML, CSS, JavaScript, Bootstrap",
-            description: `        This is a webpage I decided to make after finishing my work in my Coding Class. I was just
+      BooImg: "/images/Screenshot_2018-12-21 Tic Tac BOO.jpg",
+      BooLink: "https://dannyp123.github.io/Tic-Tac-BOO/"
+    },
+    {
+      title: "Temp-Converting",
+      projectLink: "https://dannyp123.github.io/Temp-Converting/",
+      langs: "HTML, CSS, JavaScript, Bootstrap",
+      description: `        This is a webpage I decided to make after finishing my work in my Coding Class. I was just
         learning JavaScript and wanted
         to apply it to my work so that I could further my knowledge. Although, my instructor did
         assist
-        me some, but I finished it on my own.`,
-        },
-        {
-            title: "Github-Full-Project",
-            projectLink: "https://github.com/Dannyp123/github-full-project",
-            langs: "HTML, CSS, Bootstrap",
-            description: `        This project is a piece of a Github webpage that I recreated using HTML, CSS, and
+        me some, but I finished it on my own.`
+    },
+    {
+      title: "Github-Full-Project",
+      projectLink: "https://github.com/Dannyp123/github-full-project",
+      langs: "HTML, CSS, Bootstrap",
+      description: `        This project is a piece of a Github webpage that I recreated using HTML, CSS, and
             Bootstrap. The goal was to use these tools
             to make it look as close as the original`
-        },
-        {
-            title: "Guessing-game",
-            projectLink: "https://github.com/Dannyp123/Guessing-game",
-            langs: "Python",
-            description: `        This project is a Python program that I wrote at home using Visual Studio Code. I watched a video
+    },
+    {
+      title: "Guessing-game",
+      projectLink: "https://github.com/Dannyp123/Guessing-game",
+      langs: "Python",
+      description: `        This project is a Python program that I wrote at home using Visual Studio Code. I watched a video
             and saw a very
             basic version of this program, so
             I decided to recreate one, but with more complexity. To use the program, it ask you for
@@ -106,14 +105,14 @@ const PROJECTS = {
             or
             lower
             and quits if you get the correct number.`,
-            videoLink: "https://asciinema.org/a/IOFeivcRaH2c4QFkWe8Lc1VJe",
-            videoImg: "https://asciinema.org/a/IOFeivcRaH2c4QFkWe8Lc1VJe.svg"
-        },
-        {
-            title: "rental_store_danielpeterson",
-            projectLink: "https://github.com/Dannyp123/rental_store_danielpeterson",
-            langs: " Python",
-            description: `        This project is a Python program that I made in my coding class, which uses Visual Studio Code, for
+      videoLink: "https://asciinema.org/a/IOFeivcRaH2c4QFkWe8Lc1VJe",
+      videoImg: "https://asciinema.org/a/IOFeivcRaH2c4QFkWe8Lc1VJe.svg"
+    },
+    {
+      title: "rental_store_danielpeterson",
+      projectLink: "https://github.com/Dannyp123/rental_store_danielpeterson",
+      langs: " Python",
+      description: `        This project is a Python program that I made in my coding class, which uses Visual Studio Code, for
             the final project
             in
             Python. It simulates tool rental
@@ -121,9 +120,8 @@ const PROJECTS = {
             deposit
             for
             a rental fee.`,
-            videoLink: "https://asciinema.org/a/XVRI0ApfqB2HUdQDko7Rsg2cI",
-            videoImg: "https://asciinema.org/a/XVRI0ApfqB2HUdQDko7Rsg2cI.svg"
-        },
-
-    ]
-}
+      videoLink: "https://asciinema.org/a/XVRI0ApfqB2HUdQDko7Rsg2cI",
+      videoImg: "https://asciinema.org/a/XVRI0ApfqB2HUdQDko7Rsg2cI.svg"
+    }
+  ]
+};
